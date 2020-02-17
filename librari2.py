23387c590bb276c5a303e36be41bb8c8049a3468
mallimuondu <mallimuondu@gmail.com>
2020-02-17 16:49:45 +0300

    i have added code

--- librari2.py
+++ librari2.py
@@ -1,4 +1,6 @@
-class library:
+import sys
+
+class Library:
     def __init__(self,listofbooks):
         self.availablebooks = listofbooks
         
@@ -18,3 +20,46 @@ class library:
     def addbook(self,returnedbooks):
         self.availablebooks.append(returnedbook)
         print("thank you for returing your borrowed book")
+        
+class Student:
+    def requestbook(self):
+        print("enter the name  of the book you like to borrow")
+        self.book = input()
+        return self.book
+    
+    def returnbook(self):
+        print("enter the name of the book you would like to return")
+        self.book = input()
+        return self.book
+        
+        
+def main():
+    library = Library(
+        ["diary of a wimpy kid","captain under pance"," the tortoice and th hare"])
+    student = Student()
+    done=False
+    
+    while done==False:
+        print('''=====LIBRARY MANU=====
+            1. display all available books
+            2. recwest a book
+            3. return a book
+            4. Exit
+            ''')
+        
+        choice = int(input("enter choice: "))
+
+        if choice == 1:
+            library.displayavailablebooks()
+
+        elif choice == 2:
+            library.borrowabook(student.requestbook())
+
+        elif choice == 3:
+            library.addbook(student.returnbook())
+
+        elif choice == 4:
+            sys.exit()
+        
+        
+main()
